658eb5992585acc3b90e1801ef646fc4100aca02
fanxb <fleyx20@outlook.com>
2022-07-18 13:31:59 +0800

    deploy:修改Dockerfile

--- build.sh
+++ build.sh
@@ -7,12 +7,7 @@ rm -rf openRenamerBackend/static/*
 touch openRenamerBackend/static/.gitkeep
 mv openRenamerFront/dist/*  openRenamerBackend/static
 
-# docker镜像打包
-if [ ! -n $0 ]; then
-  echo "请传入版本号参数,如：bash build.sh 0.8"
-  return 0
-fi
 # 单平台打包并推送
 #docker build -t fleyx/open-renamer:$0 --push .
 # 多平台打包并推送
-docker buildx build -t fleyx/open-renamer:$0 --platform linux/amd64,linux/arm64 --push .
+docker buildx build -t fleyx/open-renamer:$1 --platform linux/amd64,linux/arm64 --push .
