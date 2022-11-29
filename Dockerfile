96b4cb79890cb913cfe6f608d141b00a66fe9fd5
fanxb <fleyx20@outlook.com>
2022-11-29 23:35:40 +0800

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:lts-buster-slim
 WORKDIR /app
 COPY ./openRenamerBackend /app
-RUN chmod 777 -R /app
+RUN chmod 777 -R /app && npm config set registry=https://registry.npmmirror.com && npm install -g typescript
 ENV PORT 80
 CMD ["bash", "start.sh"]
 
--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 base=$(cd "$(dirname "$0")";pwd)
 cd $base
 docker run -it --rm --name buildOpenRenamer --user ${UID} -v $base/openRenamerFront:/opt/front node:lts-buster-slim  bash -c "cd /opt/front &&   yarn --registry https://registry.npmmirror.com && yarn build"
-docker run -it --rm --name buildOpenRenamer -v $base/openRenamerBackend:/app node:lts-buster-slim  bash -c "cd /app &&   npm config set registry=https://registry.npmmirror.com && npm install -g typescript && tsc"
 
 rm -rf openRenamerBackend/static/*
 touch openRenamerBackend/static/.gitkeep
