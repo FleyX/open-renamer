197c1b83426127fe771fb2eff43c7c5f48cee7fa
fanxb <fleyx20@outlook.com>
2022-11-29 23:52:34 +0800

    deploy:

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 base=$(cd "$(dirname "$0")";pwd)
 cd $base
 docker run -it --rm --name buildOpenRenamer --user ${UID} -v $base/openRenamerFront:/opt/front node:lts-buster-slim  bash -c "cd /opt/front &&   yarn --registry https://registry.npmmirror.com && yarn build"
+docker run -it --rm --name buildOpenRenamer --user ${UID} -v $base/openRenamerBackend:/opt/app node:lts-buster-slim  bash -c "cd /opt/app &&   yarn --registry https://registry.npmmirror.com && yarn global add typescript && yarn && tsc"
 
 rm -rf openRenamerBackend/static/*
 touch openRenamerBackend/static/.gitkeep
 mv openRenamerFront/dist/*  openRenamerBackend/static
+rm -rf openRenamerBackend/node_modules
 
 # 单平台打包并推送
 #docker build -t fleyx/open-renamer:$0 --push .
