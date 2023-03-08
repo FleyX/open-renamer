4eecb4f8328c8c6f57ddf413103a73f1028b8514
fanxb <fanxb.tl@gmail.com>
2023-03-07 23:11:34 -0500

    deploy:edit deploy shell

--- openRenamerBackend/start.sh
+++ openRenamerBackend/start.sh
@@ -1,1 +1,1 @@
-npm install -g pnpm && pnpm install && node dist/index.js
+npm install -g pnpm typescript --registry https://registry.npmmirror.com && pnpm install --registry https://registry.npmmirror.com && tsc && node dist/index.js
