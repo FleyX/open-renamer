0b7f4a155c9d5a6da6a58b8cea52a685e32fcced
fanxb <fanxb.tl@gmail.com>
2023-04-09 10:12:27 -0400

    deploy:修改部署脚本

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:lts-buster-slim
 WORKDIR /app
 COPY ./openRenamerBackend /app
-RUN chmod 777 -R /app 
+RUN chmod 777 -R /app && npm install -g pnpm typescript --registry https://registry.npmmirror.com
 ENV PORT 80
 CMD ["bash", "start.sh"]
 
--- openRenamerBackend/start.sh
+++ openRenamerBackend/start.sh
@@ -1,1 +1,1 @@
-npm install -g pnpm typescript --registry https://registry.npmmirror.com && pnpm install --registry https://registry.npmmirror.com && tsc && node dist/index.js
+pnpm install --registry https://registry.npmmirror.com && tsc && node dist/index.js
