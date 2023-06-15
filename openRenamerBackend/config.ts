f25e5d7bc710712330b90393ff94dfd5a4db7f70
fanxb <fanxb.tl@gmail.com>
2023-06-15 20:13:34 +0800

    fix:修复文件过多报错问题

--- openRenamerBackend/config.ts
+++ openRenamerBackend/config.ts
@@ -4,23 +4,24 @@ import * as path from 'path';
 const rootPath = path.resolve(__dirname, '..');
 
 let config = {
-  rootPath,
-  port: process.env.PORT ? parseInt(process.env.PORT) : 8089,
-  token: process.env.TOKEN ? process.env.TOKEN : null,
-  urlPrefix: '/openRenamer/api',
-  //是否为windows平台
-  isWindows: process.platform.toLocaleLowerCase().includes("win"),
-  bodyLimit: {
-    formLimit: '2mb',
-    urlencoded: true,
-    multipart: true,
-    formidable: {
-      uploadDir: path.join(rootPath, 'files', 'temp', 'uploads'),
-      keepExtenstions: true,
-      maxFieldsSize: 1024 * 1024
-    }
-  },
-  publicPath: new Set(["POST/public/checkToken"])
+    rootPath,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8089,
+    token: process.env.TOKEN ? process.env.TOKEN : null,
+    urlPrefix: '/openRenamer/api',
+    //是否为windows平台
+    isWindows: process.platform.toLocaleLowerCase().includes("win"),
+    bodyLimit: {
+        formLimit: '200mb',
+        jsonLimit: '200mb',
+        urlencoded: true,
+        multipart: true,
+        formidable: {
+            uploadDir: path.join(rootPath, 'files', 'temp', 'uploads'),
+            keepExtenstions: true,
+            maxFieldsSize: 1024 * 1024 * 200
+        }
+    },
+    publicPath: new Set(["POST/public/checkToken"])
 };
 
 export default config;
