851234ab765ebbdf28bbe31057266d9c02918c62
fanxb <fanxb.tl@gmail.com>
2023-06-25 22:10:12 +0800

    feat:优化集数识别

--- openRenamerBackend/entity/bo/rules/AutoRule.ts
+++ openRenamerBackend/entity/bo/rules/AutoRule.ts
@@ -58,7 +58,7 @@ export default class InsertRule implements RuleInterface {
             }
         } else if (this.type === 'eNum') {
             let lowName = file.originName.toLocaleLowerCase().replace(/ /g, '')
-                .replace(/\d+[a-df-z]/g, '')//去除4k,1080p等
+                .replace(/\d+[kp]/g, '')//去除4k,1080p等
                 .replace(/[xh]\d+/g, '')//去除x264,h264等 ;
             for (let i in eNumPatternArr) {
                 let patternRes = lowName.match(eNumPatternArr[i]);
