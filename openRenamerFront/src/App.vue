95a382835ac261bc84e2d47e52b17aad929f70c8
fanxb <fanxb.tl@gmail.com>
2024-10-30 21:48:41 +0800

    deploy:版本号更新

--- openRenamerFront/src/App.vue
+++ openRenamerFront/src/App.vue
@@ -42,7 +42,7 @@ export default {
   name: "Home",
   data() {
     return {
-      version: "1.6.4",
+      version: "1.7.1",
       latestVersion: null,
       activeIndex: location.pathname,
       showNewVersion: false
