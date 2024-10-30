6759333fb485b29f10b041d38d347f458fb3d23b
fanxb <fanxb.tl@gmail.com>
2024-10-30 20:13:55 +0800

    feat:全选支持选中文件夹

--- openRenamerFront/src/App.vue
+++ openRenamerFront/src/App.vue
@@ -42,7 +42,7 @@ export default {
   name: "Home",
   data() {
     return {
-      version: "1.6.3",
+      version: "1.6.4",
       latestVersion: null,
       activeIndex: location.pathname,
       showNewVersion: false
